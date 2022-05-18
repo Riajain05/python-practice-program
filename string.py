196ba73cf691bb823a8392486c1d64456ac88fb7
Riajain05 <100400846+Riajain05@users.noreply.github.com>
2022-05-18 06:27:33 -0700

    Add files via upload

---
+++ string.py
@@ -0,0 +1,57 @@
+"""a="Tony"
+b="tony"
+print(id(a))
+print(id(b))"""
+
+"""a=5
+b=5
+print(id(a))
+print(id(b))"""
+
+"""if id are same it means only one object is created"""
+
+# slicing
+
+"""i="hello riya jain"
+x=i[2:4]
+print(x,i)"""
+
+"""i="hello riya jain"
+x=i[:4]
+print(x,i)"""
+
+"""i="hello riya jain"
+x=i[2:]
+print(x,i)"""
+
+#spliting
+
+"""y=i.split(" ")
+print(y)"""
+
+#concantenation
+
+"""a="hello"
+b="riya"
+c=a+" "+b
+print(c)"""
+
+#multiplication
+
+a="Riya Jain"
+b=2
+c=a*b
+print(c)
+
+#program to print tar pattern
+
+"""a="*"
+i=1
+c=a*i
+for i in range(1,4,1):
+    for j in range(1,4,1):
+        print(c)"""
+    
+
+
+

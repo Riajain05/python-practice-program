070f01ca4af7e6c625dd6ec9d3b48c10b423c863
Riajain05 <100400846+Riajain05@users.noreply.github.com>
2022-05-18 06:10:39 -0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,46 @@
+#LIST
+
+"""a=[1,2,3,4,5,6,7,8,9]
+print(a)
+
+b=['riya',"priya",56,2.3]
+print(b)
+
+print(type(a))
+print(type(b))
+
+c=list("riya")
+print(c)"""
+
+# APPEND
+"""a=[1,2,3]
+print(a)
+a.append(23)
+print(a)"""
+
+#INSERT
+"""b=[1,2,3]
+print(b)
+b.insert(2,26)
+print(b)"""
+
+ 
+"""a=["riya","vallabh nagar",6260379739,"riya05962@gmail.com"]
+print(a)
+print("NAME: ",a[0])
+print("ADDRESS: ",a[1])
+print("CONTACT: ",a[2])
+print("EMAIL: ",a[3])"""
+
+
+a=[1,2,3,4,5,6,7,8,9,10]
+for i in range(len(a)):
+    print(a[i])
+x=a.index(5)
+print(x)
+
+
+
+
+
+
